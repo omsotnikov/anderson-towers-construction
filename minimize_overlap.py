97eac86d06b4a2debf507c0b54e569641c666539
Oleg Sotnikov <42975442+omsotnikov@users.noreply.github.com>
2022-10-30 21:47:31 +0500

    Update minimize_overlap.py

--- minimize_overlap.py
+++ minimize_overlap.py
@@ -86,7 +86,6 @@ def main():
         f['/parameters/delta'] = p.delta
         f['/parameters/gamma'] = p.gamma
         f['/parameters/steps'] = p.steps
-        f['/parameters/eigenvectors_file'] = p.eigenvectors
         f['/parameters/spins'] = spins
         f['/parameters/coordinates'] = coordinates
 
